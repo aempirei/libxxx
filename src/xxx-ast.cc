6edb020b82d68f6865dfe6831c0db1701971c5a7
Christopher Abad <aempirei@gmail.com>
2013-11-06 02:49:53 -0800

    .

--- src/xxx-ast.cc
+++ src/xxx-ast.cc
@@ -1,14 +1,3 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cstdint>
-#include <climits>
-#include <cassert>
-
-#include <unistd.h>
-
-#include <stdexcept>
-
 #include <xxx.hh>
 
 namespace xxx {
